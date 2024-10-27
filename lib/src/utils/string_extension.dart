f7878d968f2adc27b46ff6672e6b19d2d4ae2f92
Adrien Bonnin <adbonnin@users.noreply.github.com>
2024-10-27 11:27:43 +0100

    :tada: Initialisation de la GitHub pages avec Jekyll

--- lib/src/utils/string_extension.dart
+++
@@ -1,9 +0,0 @@
-extension StringExtension on String {
-  String removeSpecialCharacters() {
-    return replaceAll(RegExp(r'[^a-zA-ZÀ-ÖØ-öø-ÿ\s]+'), '');
-  }
-
-  String removeMultipleSpace() {
-    return replaceAll(RegExp(' +'), ' ');
-  }
-}
